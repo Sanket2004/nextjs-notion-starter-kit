cf9282acf868db806dc2a0af81cc091498011e17
Sanket Banerjee <99745967+Sanket2004@users.noreply.github.com>
2022-05-08 16:20:06 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -15,9 +15,6 @@ export default siteConfig({
 
   // open graph metadata (optional)
   description: 'Sanket Banerjee Blog Site',
-  image: 'https://github.com/Sanket2004/nextjs-notion-starter-kit/blob/main/blog_page-0001%20(1).jpg?raw=true', 
-  url:'https://github.com/Sanket2004/nextjs-notion-starter-kit/blob/main/blog_page-0001%20(1).jpg?raw=true',
-  twitterUsername: '@SanketBanerjee15',
 
   // social usernames (optional)
   twitter: 'sanketbanerjee15',

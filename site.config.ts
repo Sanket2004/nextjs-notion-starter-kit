ebe5930d144343d0feee95b3abd399e2feee8008
Sanket Banerjee <99745967+Sanket2004@users.noreply.github.com>
2022-05-08 16:08:35 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -15,7 +15,6 @@ export default siteConfig({
 
   // open graph metadata (optional)
   description: 'Sanket Banerjee Blog Site',
-  title: 'Programming Knowledge',
   content: "Learn Coding from Scratch🌐!",
   siteUrl: 'https://sanketbanerjee-blog.vercel.app/', // No trailing slash allowed!
   image: 'https://github.com/Sanket2004/nextjs-notion-starter-kit/blob/main/blog_page-0001%20(1).jpg?raw=true', // Path to your image you placed in the 'static' folder
